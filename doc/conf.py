3f4379782e1f81082210b466604921bf4a870516
Christian Kastner <ckk@debian.org>
2020-05-03 00:58:59 +0200

    Use local MathJax
    Forwarded: not-needed
    
    Gbp-Pq: Name Use-local-MathJax.patch

--- doc/conf.py
+++ doc/conf.py
@@ -75,7 +75,7 @@ if os.environ.get("NO_MATHJAX"):
     mathjax_path = ""
 else:
     extensions.append("sphinx.ext.mathjax")
-    mathjax_path = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js"
+    mathjax_path = "file:///usr/share/javascript/mathjax/MathJax.js?config=TeX-AMS_CHTML"
 
 autodoc_default_options = {"members": True, "inherited-members": True}
 
