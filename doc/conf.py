c42c9a04a4381f19db9e5abb982516d4b96f80fd
Yaroslav Halchenko <debian@onerussian.com>
2020-05-14 12:54:06 +0200

    Disable google analytics referrals in the -doc
      if we were to provide some information from just reading  documentation we
      had to disclose and seek approval from the user before doing that.
    
      Thanks Ambrose Andrews for the analysis and report
    
    Origin: Debian
    Bug: 730259
    Bug-Debian: http://bugs.debian.org/730259
    Bug-Ubuntu: https://launchpad.net/bugs/
    Last-Update: 2013-11-24
    
    Gbp-Pq: Name remove-google-analytics.patch

--- doc/conf.py
+++ doc/conf.py
@@ -142,7 +142,7 @@ html_theme = 'scikit-learn-modern'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {'google_analytics': True,
+html_theme_options = {'google_analytics': False,
                       'mathjax_path': mathjax_path}
 
 # Add any paths that contain custom themes here, relative to this directory.
