8867ba6aeead01185785cb457c4042655a573d15
Loïc Estève <loic.esteve@ymail.com>
2024-08-07 11:04:05 +0200

    Fix i686 test failure
    Forwarded: https://github.com/scikit-learn/scikit-learn/issues/27506
    Bug-Debian: https://bugs.debian.org/1070201
    
    
    Gbp-Pq: Name i386-impurity-nan.patch

--- sklearn/tree/_export.py
+++ sklearn/tree/_export.py
@@ -266,7 +266,7 @@ class _BaseTreeExporter:
             self.colors["rgb"] = _color_brew(tree.n_classes[0])
             if tree.n_outputs != 1:
                 # Find max and min impurities for multi-output
-                self.colors["bounds"] = (np.min(-tree.impurity), np.max(-tree.impurity))
+                self.colors["bounds"] = (-np.max(tree.impurity), -np.min(tree.impurity))
             elif tree.n_classes[0] == 1 and len(np.unique(tree.value)) != 1:
                 # Find max and min values in leaf nodes for regression
                 self.colors["bounds"] = (np.min(tree.value), np.max(tree.value))
